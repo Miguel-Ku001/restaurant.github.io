68190671f75ddea83c7ce0f59decb9d31ec91b4e
Invizib1e <291182@alumno.utmmetropolitana.edu.mx>
2023-09-23 15:02:53 -0600

    final de turno

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,10 +10,10 @@ export const Login = () => {
 
   return (
 
-    <div className="container-login container flex flex-row">
+    <div className="flex flex-row" >
 
-      <div className="basis-1/2">
-        <div style={{backgroundColor: '#262626'}}>
+      <div className="flex justify-center" style={{width: '50%',backgroundColor: '#262626'}}>
+        <div>
 
           <div>
 
@@ -28,13 +28,31 @@ export const Login = () => {
 
           </div>
           <div>
-            <h1 className="text-4xl font-semibold">Bienvenidos</h1>
+            <h1 className="text-4xl font-semibold" style={{color: '#f8fafc'}}>Bienvenido</h1>
           </div>
-          <div>3</div>
+          <div>
+            <h1 style={{color: '#f8fafc'}}>Iniciar Sesion</h1>
+            <div>
+              <form action="">
+
+                <input
+                  type="email"
+                  className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
+                  id="floatingInput"
+                  placeholder="name@example.com" />
+                <label
+                  for="floatingInput"
+                  className="pointer-events-none absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >Email address</label>
+
+                <input className="rounded" type="text" placeholder="Ingresa tu usuario" style={{height: '46px', width: '250px', marginBottom: '10px',paddingLeft: '5px'}}/>
+              </form>
+            </div>
+          </div>
 
         </div>
       </div>
-      <div className="basis-1/2" style={{backgroundColor: 'red'}}>San Roman</div>
+      <div className="flex justify-center" style={{backgroundColor: 'red', width: '50%'}}>San Roman</div>
 
     </div>
 
