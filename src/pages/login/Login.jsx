0d137199179ef942ccb51578951fc7a446301b09
Invizib1e <291182@alumno.utmmetropolitana.edu.mx>
2023-09-23 13:26:28 -0600

    inicio login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,41 @@
+
 export const Login = () => {
+
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+
   return (
-    <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-10 lg:px-8">Bienvenido a login</div>
+
+    <div className="container-login container flex flex-row">
+
+      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-10 lg:px-8">
+        <div>
+
+          <div>
+
+            <img
+              src={user.imageUrl}
+              className="avatar"
+              style={{
+                width: user.imageSize,
+                height: user.imageSize
+              }}
+            />
+
+          </div>
+          <div>2</div>
+          <div>3</div>
+
+        </div>
+      </div>
+      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-10 lg:px-8">San Roman</div>
+
+    </div>
+
+    
   )
 }
 export default Login
