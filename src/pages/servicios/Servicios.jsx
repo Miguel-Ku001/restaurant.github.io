55d0a427b0194c1adf49ef937f002d3bcf9aa069
Nicolas <chaveznicolas3701@gmail.com>
2023-09-24 15:45:39 -0600

    Correccion del maquetado segun los cambios a la vista de Servicios

--- src/pages/servicios/Servicios.jsx
+++ src/pages/servicios/Servicios.jsx
@@ -1,6 +1,6 @@
 
 
-import {Card, Image, CardBody, Button} from "@nextui-org/react";
+import {Card, Image, CardBody, Button, Link} from "@nextui-org/react";
 
 export const Servicios = () => {
   return (
@@ -25,53 +25,64 @@ export const Servicios = () => {
 
 
       <div className="mx-auto w-2/5 h-auto py-14">
-        <p className="text-center text-2xl text-gray-800">Descubra nuestros servicios especializados que se adaptan a sus necesidades. Desde consultoría estratégica hasta soluciones a medida, estamos aquí para potenciar su éxito.</p>
+        <h2 className="text-4xl text-center text-gray-800 pb-4">Descubrelos</h2>
+        <p className="text-center text-2xl text-gray-800">Nuestros servicios especializados que se adaptan a sus necesidades. Desde consultoría estratégica hasta soluciones a medida, estamos aquí para potenciar su éxito.</p>
       </div>
 
       <div className="mx-auto w-4/6 h-auto py-14 grid grid-cols-3 gap-20">
         <div className="z-0 grid place-items-center">
           <div className="">
-            <Button className="h-fit w-60 p-0 rounded-2xl">
-              <Image
-                isZoomed
-                width={240}
-                alt="NextUI Fruit Image with Zoom"
-                src="/src/images/imagen2-servicios.png"
-              />
-            </Button>
+            <Image
+              isZoomed
+              width={240}
+              alt="NextUI Fruit Image with Zoom"
+              src="/src/images/imagen2-servicios.png"
+            />
           </div>
-            <h3 className="h-8 text-center text-2xl text-gray-800 my-4">Eventos</h3>
-            <p className="h-44 text-justify text-base text-gray-800 px-14"> Nuestros servicios para eventos ofrecen opciones de menús personalizados y atención de primera clase para que su ocasión especial sea verdaderamente única. </p>
+          <h3 className="h-8 text-center text-2xl text-gray-800 my-4">EVENTOS</h3>
+          <p className="h-44 text-justify text-base text-gray-800 px-14"> Nuestros servicios para eventos ofrecen opciones de menús personalizados y atención de primera clase para que su ocasión especial sea verdaderamente única. 
+            <br/>
+            <Link color="foreground" name="Sucursales" href="/servicios/eventos" className="text-gray-800 mt-4">
+              Saber más...
+            </Link>
+          </p>
+          
         </div>
 
         <div className="z-0 grid place-items-center">
           <div className="">
-            <Button className="h-fit w-60 p-0 rounded-2xl">
-              <Image
-                isZoomed
-                width={240}
-                alt="NextUI Fruit Image with Zoom"
-                src="/src/images/imagen3-servicios.png"
-              />
-            </Button>
+            <Image
+              isZoomed
+              width={240}
+              alt="NextUI Fruit Image with Zoom"
+              src="/src/images/imagen3-servicios.png"
+            />
           </div>
-            <h3 className="h-8 text-center text-2xl text-gray-800 my-4">Bodas</h3>
-            <p className="h-44 text-justify text-base text-gray-800 px-14"> Un banquete de boda de ensueño: En nuestro restaurante, hacemos que su día especial sea aún más mágico con servicios personalizados para bodas.  </p>
+            <h3 className="h-8 text-center text-2xl text-gray-800 my-4">BODAS</h3>
+            <p className="h-44 text-justify text-base text-gray-800 px-14"> Un banquete de boda de ensueño: En nuestro restaurante, hacemos que su día especial sea aún más mágico con servicios personalizados para bodas.  
+              <br/>
+              <Link color="foreground" name="Sucursales" href="/servicios/bodas" className="text-gray-800 mt-4">
+                Saber más...
+              </Link>
+            </p>
         </div>
 
         <div className="z-0 grid place-items-center">
           <div className="">
-            <Button className="h-fit w-60 p-0 rounded-2xl">
-              <Image
-                isZoomed
-                width={240}
-                alt="NextUI Fruit Image with Zoom"
-                src="/src/images/imagen4-servicios.png"
-              />
-            </Button>
+            <Image
+              isZoomed
+              width={240}
+              alt="NextUI Fruit Image with Zoom"
+              src="/src/images/imagen4-servicios.png"
+            />
           </div>
-            <h3 className="h-8 text-center text-2xl text-gray-800 my-4">Catering</h3>
-            <p className="h-44 text-justify text-base text-gray-800 px-14"> Catering excepcional: Nuestros servicios de catering brindan una experiencia culinaria sobresaliente en cualquier lugar que elija. </p>
+            <h3 className="h-8 text-center text-2xl text-gray-800 my-4">CATERING</h3>
+            <p className="h-44 text-justify text-base text-gray-800 px-14"> Catering excepcional: Nuestros servicios de catering brindan una experiencia culinaria sobresaliente en cualquier lugar que elija.
+              <br/>
+              <Link color="foreground" name="Sucursales" href="/servicios/catering" className="text-gray-800 mt-4">
+                Saber más...
+              </Link>
+            </p>
         </div>
 
       </div>
