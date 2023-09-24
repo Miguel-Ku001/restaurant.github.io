7cafc7a3f846a82f971a3916ab2dbbbf9daa9686
JULIO <juliogonzalez99912@gmail.com>
2023-09-23 22:16:31 -0600

    prueba de vista

--- src/pages/catering/Catering.jsx
+++ src/pages/catering/Catering.jsx
@@ -1,6 +1,26 @@
+import React from "react";
+
+import {Select, SelectItem} from "@nextui-org/react";
+
+
+
 export const Catering = () => {
     return (
-      <div>Bienvenido a Catering</div>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <Select 
+        label="Select an animal" 
+        className="max-w-xs" 
+      >
+        
+      </Select>
+      <Select
+        label="Favorite Animal"
+        placeholder="Select an animal"
+        className="max-w-xs"
+      >
+       
+      </Select>
+    </div>
     )
   }
   
