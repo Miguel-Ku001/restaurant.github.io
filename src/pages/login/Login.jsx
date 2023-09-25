5cb5ae726f667c52beebd7718ce021359c8d0e58
Invizib1e <291182@alumno.utmmetropolitana.edu.mx>
2023-09-25 16:48:26 -0600

    edicion login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,7 @@ export const Login = () => {
 
   const user = {
     name: 'Hedy Lamarr',
-    imageUrl: '',
+    imageUrl: './images/LOGO3.png',
     imageSize: 90,
   };
 
@@ -31,21 +31,50 @@ export const Login = () => {
             <h1 className="text-4xl font-semibold" style={{color: '#f8fafc'}}>Bienvenido</h1>
           </div>
           <div>
-            <h1 style={{color: '#f8fafc'}}>Iniciar Sesion</h1>
-            <div>
+            <h1 style={{color: '#f8fafc', marginTop: '15px', }}>Iniciar Sesion</h1>
+            <div className="flex flex-column">
               <form action="">
 
-                <input
-                  type="email"
-                  className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
-                  id="floatingInput"
-                  placeholder="name@example.com" />
-                <label
-                  for="floatingInput"
-                  className="pointer-events-none absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                >Email address</label>
+                <div style={{marginTop: '15px'}}>
+                  <div className="w-72">
+                    <div className="relative h-10 w-full min-w-[200px]">
+                      <input
+                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        placeholder=" "
+                      />
+                      <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500" style={{color: 'white'}}>
+                        Ingresar Usuario
+                      </label>
+                    </div>
+                  </div>
+                </div>
 
-                <input className="rounded" type="text" placeholder="Ingresa tu usuario" style={{height: '46px', width: '250px', marginBottom: '10px',paddingLeft: '5px'}}/>
+                <div style={{marginTop: '15px'}}>
+                  <div className="w-72">
+                    <div className="relative h-10 w-full min-w-[200px]">
+                      <input
+                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        placeholder=" "
+                      />
+                      <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500" style={{color: 'white'}}>
+                        Ingresar Contraseña
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                {/*
+                <div style={{marginTop: '15px'}}>
+                  <input className="rounded" type="text" placeholder="Ingresa tu usuario" style={{height: '46px', width: '350px', marginBottom: '10px',paddingLeft: '5px'}}/>
+                </div>
+                <div>
+                <input className="rounded" type="text" placeholder="Ingresa tu usuario" style={{height: '46px', width: '350px', marginBottom: '10px',paddingLeft: '5px'}}/>
+                </div>
+            */}
+                <div>
+                  <button className="rounded-xl" style={{width: '110px',padding: '4px',border:'none',marginTop:'12px', backgroundColor: '#CD9B4A', color: 'White'}}>Ingresar</button>
+                </div>    
+            
               </form>
             </div>
           </div>
