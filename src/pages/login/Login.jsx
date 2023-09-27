a9e5a8c0c5e68fcf2249326ee6c2271f55d71f87
Invizib1e <291182@alumno.utmmetropolitana.edu.mx>
2023-09-26 20:26:17 -0600

    Final01

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,37 +3,37 @@ import {Image} from "@nextui-org/react";
 export const Login = () => {
   return (
 
-    <div className="flex flex-row" >
+    <div className="flex flex-row">
 
       <div className="flex justify-center" style={{width: '50%',backgroundColor: '#262626', backgroundSize: '100%'}}>
         <div>
           
-          <div>
+          <div className=" flex justify-center">
             <Image
-              className="items-center"
-              width={200}
+   
+              width={300}
               src="/src/images/LOGO3.png"
             />
           </div>
 
           <div>
-            <h1 className="text-4xl font-semibold" style={{color: '#f8fafc'}}>Bienvenido</h1>
+            <h1 className="text-4xl  text-center font-serif" style={{color: '#f8fafc', marginTop:'-50px'}}>BIENVENIDO</h1>
           </div>
 
           <div>
-            <h1 style={{color: '#f8fafc', marginTop: '15px', }}>Iniciar Sesion</h1>
+            <h1 style={{color: '#f8fafc', marginTop: '15px', }}>Iniciar Sesión</h1>
             <div className="flex flex-column">
               <form action="">
 
-                <div style={{marginTop: '15px'}}>
+                <div style={{marginTop: '5px'}}>
                   <div className="w-72">
                     <div className="relative h-10 w-full min-w-[200px]">
                       <input
-                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-orange-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        className="text-white font-semibold peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-orange-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         placeholder=" "
                       />
                       <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-orange-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-orange-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-orange-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500" style={{color: 'white'}}>
-                        Ingresar Usuario
+                        Ingresar tu usuario
                       </label>
                     </div>
                   </div>
@@ -43,11 +43,11 @@ export const Login = () => {
                   <div className="w-72">
                     <div className="relative h-10 w-full min-w-[200px]">
                       <input
-                        className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-orange-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        className="text-white font-semibold peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-orange-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         placeholder=" "
                       />
                       <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-orange-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-orange-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-orange-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500" style={{color: 'white'}}>
-                        Ingresar Contraseña
+                        Ingresar tu contraseña
                       </label>
                     </div>
                   </div>
@@ -75,10 +75,9 @@ export const Login = () => {
       <div className="flex justify-center" style={{width: '50%'}}>
         <div>
           
-          <div>
+          <div className="sticky">
             <Image
               className="items-center"
-              width={'100%'}
               src="/src/images/loginbg.jpg"
             /> 
           </div>  
