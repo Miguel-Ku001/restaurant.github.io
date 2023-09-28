fc794040dc41c4e198df1f71bf3ec968d20997ae
Angeles <angelestun3@gmail.com>
2023-09-28 13:39:21 -0600

    se agregaron las imágenes e información

--- src/pages/sucursales/Sucursales.jsx
+++ src/pages/sucursales/Sucursales.jsx
@@ -1,6 +1,194 @@
+import {Image} from "@nextui-org/react";
 export const Sucursales = () => {
   return (
-    <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-10 lg:px-8">Bienvenido a Sucursales</div>
+    <div className="flex items-center justify-center flex-col p-4 mx-auto mb-64">
+      <div className="mx-auto w-2/5 h-auto">
+        <h2 className="mx-auto text-center w-40 py-1.5 pt-8 px-4 text-4xl text-gray-800">SUCURSALES</h2>
+      </div>
+
+
+      <div className="mx-auto h-auto w-1/2 z-0">
+        <div className="flex items-center justify-center flex-col mt-8">
+        </div>
+
+        <div className="flex items-center justify-center">
+
+          <div className="w-2/5">
+            <Image
+              width={3170}
+              src="/src/images/vista-sucursales/imagen1-sucursales.jpg"
+              radius="none"
+            />
+          </div>
+
+          <div className="mx-10 w-4 flex justify-center">
+            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
+          </div>
+
+          <div className="w-2/5">
+            <h3 className="w-35 text-2xl text-gray-800">MONTEJO</h3>
+            <p className="mb-2 text-gray-800">
+            Calle 5 Diag #476, Fracc. Gonzalo Guerrero. C.P. 97118
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de restaurante:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Miercoles: 13:00 a 00:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Jueves - Sabado: 13:00 a 02:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Domingo: 12:00 a 00:00 hrs.
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de servicio a domicilio:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Domingo: 12:00 a 22:30 hrs.
+            </p>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto h-auto w-1/2 z-0">
+        <div className="flex items-center justify-center flex-col mt-8">
+        </div>
+
+        <div className="flex items-center justify-center">
+
+          <div className="w-2/5">
+            <Image
+              width={3170}
+              src="/src/images/vista-sucursales/imagen2-sucursales.jpg"
+              radius="none"
+            />
+          </div>
+
+          <div className="mx-10 w-4 flex justify-center">
+            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
+          </div>
+
+          <div className="w-2/5">
+            <h3 className="w-35 text-2xl text-gray-800">ALTABRISA</h3>
+            <p className="mb-2 text-gray-800">
+            Calle 18 #499 x7 Colonia Maya. C.P. 97134
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de restaurante:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Miercoles: 13:00 a 00:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Jueves - Sabado: 13:00 a 02:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Domingo: 12:00 a 00:00 hrs.
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de servicio a domicilio:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Domingo: 12:00 a 22:30 hrs.
+            </p>  
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto h-auto w-1/2 z-0">
+        <div className="flex items-center justify-center flex-col mt-8">
+        </div>
+
+        <div className="flex items-center justify-center">
+
+          <div className="w-2/5">
+            <Image
+              width={3170}
+              src="/src/images/vista-sucursales/imagen3-sucursales.jpg"
+              radius="none"
+            />
+          </div>
+
+          <div className="mx-10 w-4 flex justify-center">
+            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
+          </div>
+
+          <div className="w-2/5">
+            <h3 className="w-35 text-2xl text-gray-800">CARMEN</h3>
+            <p className="mb-2 text-gray-800">
+            Av. Concordia s/n entre 58 y Universidad Col. Petrolera. C.P. 24179 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de restaurante:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Miercoles: 13:00 a 00:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Jueves - Sabado: 13:00 a 02:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Domingo: 12:00 a 00:00 hrs.
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de servicio a domicilio:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Domingo: 12:00 a 22:30 hrs.
+            </p>  
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto h-auto w-1/2 z-0">
+        <div className="flex items-center justify-center flex-col mt-8">
+        </div>
+
+        <div className="flex items-center justify-center">
+
+          <div className="w-2/5">
+            <Image
+              width={3170}
+              src="/src/images/vista-sucursales/imagen4-sucursales.jpg"
+              radius="none"
+            />
+          </div>
+
+          <div className="mx-10 w-4 flex justify-center">
+            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
+          </div>
+
+          <div className="w-2/5">
+            <h3 className="w-35 text-2xl text-gray-800">CIUDAD CAUCEL</h3>
+            <p className="mb-2 text-gray-800">
+            Av. Tablaje 40353 s/n Col. Caucel, Mérida, Yucatán. C.P. 97314
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de restaurante:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Miercoles: 13:00 a 00:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Jueves - Sabado: 13:00 a 02:00 hrs. 
+            </p>
+            <p className="mb-2 text-gray-800">
+            Domingo: 12:00 a 00:00 hrs.
+            </p>
+            <p className="mb-2 text-gray-800">
+            Horario de servicio a domicilio:
+            </p>
+            <p className="mb-2 text-gray-800">
+            Lunes - Domingo: 12:00 a 22:30 hrs.
+            </p>  
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Sucursales
