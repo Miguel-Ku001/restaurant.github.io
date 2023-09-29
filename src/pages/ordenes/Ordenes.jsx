f9569383b7ba53c9cc9c91ff1b37a40c897d97f7
Nicolas <chaveznicolas3701@gmail.com>
2023-09-29 02:37:50 -0600

    Vista Ordenes terminada con pequeños cambios en los inputs de cantidad y precio

--- src/pages/ordenes/Ordenes.jsx
+++ src/pages/ordenes/Ordenes.jsx
@@ -1,4 +1,4 @@
-import {Input, Textarea} from "@nextui-org/react";
+import {Input, Textarea, Button, Link} from "@nextui-org/react";
 
 export const Ordenes = () => {
     return (
@@ -7,7 +7,7 @@ export const Ordenes = () => {
           <div>
             <h2 className="text-4xl text-center text-gray-800 pb-4 font-medium">Ordenar</h2>
           </div>
-          <div>
+          <div className="flex">
             <div className="w-1/2 py-2 pr-10">
               <div>
                 <h4 className="text-xl  text-gray-800">Nombre y apellido*</h4>
@@ -41,7 +41,185 @@ export const Ordenes = () => {
             </div>
 
 
-            <div className="w-1/2"></div>
+            <div className="w-1/2 py-2 pl-10">
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-4">
+                <h4 className="text-xl  text-gray-800">Producto</h4>
+                <h4 className="text-xl  text-gray-800">Cantidad</h4>
+                <h4 className="text-xl  text-gray-800">Precio</h4>
+              </div>
+
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-6">
+                <p className="text-lg  text-gray-800">Taco de pastor</p>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  min="0" 
+                  max="1000"
+                  variant="bordered"
+                />
+
+                <Input
+                  placeholder="0.00"
+                  startContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">$</span>
+                    </div>
+                  }
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">MXN</span>
+                    </div>
+                  }
+                  type="number"
+                  isReadOnly
+                />
+              </div>
+
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-6">
+                <p className="text-lg  text-gray-800">Torta al pastor</p>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  min="0" 
+                  max="1000"
+                  variant="bordered"
+                />
+
+                <Input
+                  placeholder="0.00"
+                  startContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">$</span>
+                    </div>
+                  }
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">MXN</span>
+                    </div>
+                  }
+                  type="number"
+                  isReadOnly
+                />
+              </div>
+
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-6">
+                <p className="text-lg  text-gray-800">Quesadilla de pastor</p>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  min="0" 
+                  max="1000"
+                  variant="bordered"
+                />
+
+                <Input
+                  placeholder="0.00"
+                  startContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">$</span>
+                    </div>
+                  }
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">MXN</span>
+                    </div>
+                  }
+                  type="number"
+                  isReadOnly
+                />
+              </div>
+
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-6">
+                <p className="text-lg  text-gray-800">Burrito al pastor</p>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  min="0" 
+                  max="1000"
+                  variant="bordered"
+                />
+
+                <Input
+                  placeholder="0.00"
+                  startContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">$</span>
+                    </div>
+                  }
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">MXN</span>
+                    </div>
+                  }
+                  type="number"
+                  isReadOnly
+                />
+              </div>
+
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-6">
+                <p className="text-lg  text-gray-800">Nachos al pastor</p>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  min="0" 
+                  max="1000"
+                  variant="bordered"
+                />
+
+                <Input
+                  placeholder="0.00"
+                  startContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">$</span>
+                    </div>
+                  }
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">MXN</span>
+                    </div>
+                  }
+                  type="number"
+                  isReadOnly
+                />
+              </div>
+
+              <div className="grid grid-cols-3 gap-4 mt-2 mb-6">
+                <p className="text-lg  text-gray-800">Sopes al pastor</p>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  min="0" 
+                  max="1000"
+                  variant="bordered"
+                />
+
+                <Input
+                  placeholder="0.00"
+                  startContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">$</span>
+                    </div>
+                  }
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">MXN</span>
+                    </div>
+                  }
+                  type="number"
+                  isReadOnly
+                />
+              </div>
+
+              <div className="flex flex-row-reverse">
+                <div className="w-1/4">
+                  <Button as={Link} href="#" fullWidth size="lg" radius="sm" variant="solid" className="text-white bg-amber-600">
+                    Ordenar
+                  </Button>
+                </div>
+              </div>
+
+            </div>
+
           </div>
         </div>
       </div>
