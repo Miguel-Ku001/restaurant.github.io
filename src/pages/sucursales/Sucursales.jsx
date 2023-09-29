0cd4c8efbfe37db99d82845415dde14ce3f330cd
Angeles <angelestun3@gmail.com>
2023-09-28 19:17:08 -0600

    se agregaron cambios de margen

--- src/pages/sucursales/Sucursales.jsx
+++ src/pages/sucursales/Sucursales.jsx
@@ -1,33 +1,24 @@
-import {Image} from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 export const Sucursales = () => {
   return (
-    <div className="flex items-center justify-center flex-col p-4 mx-auto mb-64">
-      <div className="mx-auto w-2/5 h-auto">
-        <h2 className="mx-auto text-center w-40 py-1.5 pt-8 px-4 text-4xl text-gray-800">SUCURSALES</h2>
-      </div>
+    <div className="flex flex-col items-center p-4 mx-auto mb-64">
+      <h2 className="text-center py-1.5 pt-8 text-4xl text-gray-800 mb-8">SUCURSALES</h2>
 
-
-      <div className="mx-auto h-auto w-1/2 z-0">
-        <div className="flex items-center justify-center flex-col mt-8">
+      <div className="flex flex-row justify-between w-full p-4">
+        <div className="flex flex-col items-center p-4 w-1/2">
+          <Image
+            width={280}
+            height={280}
+            src="/src/images/vista-sucursales/imagen1-sucursales.jpg"
+            radius="none"
+          />
         </div>
 
-        <div className="flex items-center justify-center">
-
-          <div className="w-2/5">
-            <Image
-              width={3170}
-              src="/src/images/vista-sucursales/imagen1-sucursales.jpg"
-              radius="none"
-            />
-          </div>
-
-          <div className="mx-10 w-4 flex justify-center">
-            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
-          </div>
-
-          <div className="w-2/5">
-            <h3 className="w-35 text-2xl text-gray-800">MONTEJO</h3>
-            <p className="mb-2 text-gray-800">
+        <div className="flex flex-col items-left p-4 w-4/5">
+          <h3 className="w-35 text-2xl text-gray-800 text-left">
+            MONTEJO
+          </h3>
+          <p className="mb-2 text-gray-800">
             Calle 5 Diag #476, Fracc. Gonzalo Guerrero. C.P. 97118
             </p>
             <p className="mb-2 text-gray-800">
@@ -48,32 +39,24 @@ export const Sucursales = () => {
             <p className="mb-2 text-gray-800">
             Lunes - Domingo: 12:00 a 22:30 hrs.
             </p>
-            
-          </div>
         </div>
       </div>
 
-      <div className="mx-auto h-auto w-1/2 z-0">
-        <div className="flex items-center justify-center flex-col mt-8">
+      <div className="flex flex-row justify-between w-full p-4">
+        <div className="flex flex-col items-center p-4 w-1/2">
+          <Image
+            width={280}
+            height={280}
+            src="/src/images/vista-sucursales/imagen2-sucursales.jpg"
+            radius="none"
+          />
         </div>
 
-        <div className="flex items-center justify-center">
-
-          <div className="w-2/5">
-            <Image
-              width={3170}
-              src="/src/images/vista-sucursales/imagen2-sucursales.jpg"
-              radius="none"
-            />
-          </div>
-
-          <div className="mx-10 w-4 flex justify-center">
-            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
-          </div>
-
-          <div className="w-2/5">
-            <h3 className="w-35 text-2xl text-gray-800">ALTABRISA</h3>
-            <p className="mb-2 text-gray-800">
+        <div className="flex flex-col items-left p-4 w-4/5">
+          <h3 className="w-35 text-2xl text-gray-800 text-left">
+            ALTABRISA
+          </h3>
+          <p className="mb-2 text-gray-800">
             Calle 18 #499 x7 Colonia Maya. C.P. 97134
             </p>
             <p className="mb-2 text-gray-800">
@@ -94,31 +77,24 @@ export const Sucursales = () => {
             <p className="mb-2 text-gray-800">
             Lunes - Domingo: 12:00 a 22:30 hrs.
             </p>  
-          </div>
         </div>
       </div>
 
-      <div className="mx-auto h-auto w-1/2 z-0">
-        <div className="flex items-center justify-center flex-col mt-8">
+      <div className="flex flex-row justify-between w-full p-4">
+        <div className="flex flex-col items-center p-4 w-1/2">
+          <Image
+            width={280}
+            height={280}
+            src="/src/images/vista-sucursales/imagen3-sucursales.jpg"
+            radius="none"
+          />
         </div>
 
-        <div className="flex items-center justify-center">
-
-          <div className="w-2/5">
-            <Image
-              width={3170}
-              src="/src/images/vista-sucursales/imagen3-sucursales.jpg"
-              radius="none"
-            />
-          </div>
-
-          <div className="mx-10 w-4 flex justify-center">
-            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
-          </div>
-
-          <div className="w-2/5">
-            <h3 className="w-35 text-2xl text-gray-800">CARMEN</h3>
-            <p className="mb-2 text-gray-800">
+        <div className="flex flex-col items-left p-4 w-4/5">
+          <h3 className="w-35 text-2xl text-gray-800 text-left">
+            CARMEN
+          </h3>
+          <p className="mb-2 text-gray-800">
             Av. Concordia s/n entre 58 y Universidad Col. Petrolera. C.P. 24179 
             </p>
             <p className="mb-2 text-gray-800">
@@ -139,31 +115,24 @@ export const Sucursales = () => {
             <p className="mb-2 text-gray-800">
             Lunes - Domingo: 12:00 a 22:30 hrs.
             </p>  
-          </div>
         </div>
       </div>
 
-      <div className="mx-auto h-auto w-1/2 z-0">
-        <div className="flex items-center justify-center flex-col mt-8">
+      <div className="flex flex-row justify-between w-full p-4">
+        <div className="flex flex-col items-center p-4 w-1/2">
+          <Image
+            width={280}
+            height={280}
+            src="/src/images/vista-sucursales/imagen4-sucursales.jpg"
+            radius="none"
+          />
         </div>
 
-        <div className="flex items-center justify-center">
-
-          <div className="w-2/5">
-            <Image
-              width={3170}
-              src="/src/images/vista-sucursales/imagen4-sucursales.jpg"
-              radius="none"
-            />
-          </div>
-
-          <div className="mx-10 w-4 flex justify-center">
-            <img className="w-0.4" src="/src/images/vista-sucursales/linea-vertical.png" alt="Linea separadora" />
-          </div>
-
-          <div className="w-2/5">
-            <h3 className="w-35 text-2xl text-gray-800">CIUDAD CAUCEL</h3>
-            <p className="mb-2 text-gray-800">
+        <div className="flex flex-col items-left p-4 w-4/5">
+          <h3 className="w-35 text-2xl text-gray-800 text-left">
+            CIUDAD CAUCEL
+          </h3>
+          <p className="mb-2 text-gray-800">
             Av. Tablaje 40353 s/n Col. Caucel, Mérida, Yucatán. C.P. 97314
             </p>
             <p className="mb-2 text-gray-800">
@@ -184,11 +153,11 @@ export const Sucursales = () => {
             <p className="mb-2 text-gray-800">
             Lunes - Domingo: 12:00 a 22:30 hrs.
             </p>  
-            
-          </div>
         </div>
       </div>
+   
     </div>
   )
 }
+
 export default Sucursales
