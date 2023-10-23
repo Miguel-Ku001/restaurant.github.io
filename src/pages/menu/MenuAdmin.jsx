425da9eb462e4b509bbd8dda4924b1b19868e554
Nicolas <chaveznicolas3701@gmail.com>
2023-10-23 05:11:10 -0600

    Update semi completado, faltan las imagenes

--- src/pages/menu/MenuAdmin.jsx
+++ src/pages/menu/MenuAdmin.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Select, SelectItem, 
         Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const MenuAdmin = () => {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const { isOpen: isUpdateModalOpen, onOpen: openUpdateModal, onClose: closeUpdateModal } = useDisclosure();
     const [nombre, setNombre] = useState('')
     const [desc, setDesc] = useState('')
     const [precio, setPrecio] = useState('')
     const [img, setImg] = useState('')
     const [cat, setCat] = useState('')
+    //const {id_items} = useParams();
     const navigate = useNavigate();
 
     const formData = new FormData();
+    //formData.append('id', id);
     formData.append('nombre', nombre);
     formData.append('desc', desc);
     formData.append('precio', precio);
@@ -23,6 +26,46 @@ export const MenuAdmin = () => {
     function handleSubmit(event) {
         event.preventDefault();
         axios.post('/api/items/crear', formData)
+        .then(res => {
+            console.log(res);
+            navigate('/menu/admin');
+        }).catch(err => console.log(err));
+    }
+
+
+    //ACTUALIZAR REGISTROS 
+    const [registroActual, setRegistroActual] = useState(null);
+    const [nombreActualizado, setNombreActualizado] = useState('');
+    const [descActualizado, setDescActualizado] = useState('');
+    const [precioActualizado, setPrecioActualizado] = useState('');
+    const [imgActualizado, setImgActualizado] = useState('');
+    const [catActualizado, setCatActualizado] = useState('');
+
+    const updatedFormData = new FormData();
+
+    updatedFormData.append('nombre', nombreActualizado);
+    updatedFormData.append('desc', descActualizado);
+    updatedFormData.append('precio', precioActualizado);
+    //updatedFormData.append('img', img);
+    updatedFormData.append('img', imgActualizado);
+    updatedFormData.append('cat', catActualizado);
+
+    const handleActualizarClick = (registro) => {
+        setRegistroActual(registro);
+
+        setNombreActualizado(registro.nombre);
+        setDescActualizado(registro.descripcion);
+        setPrecioActualizado(registro.precio);
+        setImgActualizado(registro.imagen);
+        setCatActualizado(registro.idcategoria);
+
+        openUpdateModal();
+    };
+
+
+    function handleUpdate(event) {
+        event.preventDefault();
+        axios.put(`/api/items/actualizar/${registroActual.id_items}`, updatedFormData)
         .then(res => {
             console.log(res);
             navigate('/menu/admin');
@@ -152,9 +195,94 @@ export const MenuAdmin = () => {
                                     <TableCell>{`$` + data.precio}</TableCell>
                                     <TableCell>{data.nombre_categoria}</TableCell>
                                     <TableCell className="grid grid-cols-2 gap-2">
-                                        <Button size="sm" color="primary">
-                                            Actualizar
-                                        </Button>
+                                        <>
+                                            <Button size="sm" color="primary" onClick={() => handleActualizarClick(data)}>Actualizar</Button>
+                                            <Modal isOpen={isUpdateModalOpen} onOpenChange={closeUpdateModal} size="4xl">
+                                                <ModalContent>
+                                                {(onClose) => (
+                                                    <>
+                                                    <ModalHeader className="flex flex-col gap-1 mt-5 mx-10">Actualizar</ModalHeader>
+
+                                                    <ModalBody className="mx-10">
+                                                        <div className="grid grid-cols-2 gap-4">
+                                                            <Input
+                                                                label="Actualizar nombre"
+                                                                placeholder="Tacos de..."
+                                                                labelPlacement="outside"
+                                                                variant="bordered"
+                                                                value={nombreActualizado}
+                                                                onChange={e => setNombreActualizado(e.target.value)}
+                                                            />
+                                                            <Input
+                                                                label="Actualizar descripción"
+                                                                placeholder="Tortilla de maíz o..."
+                                                                labelPlacement="outside"
+                                                                variant="bordered"
+                                                                value={descActualizado}
+                                                                onChange={e => setDescActualizado(e.target.value)}
+                                                            />
+                                                        </div>
+                                                        <div className="grid grid-cols-2 gap-4 mt-2 mb-6">
+                                                            <Input
+                                                                type="number"
+                                                                label="Actualizar precio"
+                                                                placeholder="0.00"
+                                                                labelPlacement="outside"
+                                                                variant="bordered"
+                                                                value={precioActualizado}
+                                                                onChange={e => setPrecioActualizado(e.target.value)}
+                                                                startContent={
+                                                                    <div className="pointer-events-none flex items-center">
+                                                                        <span className="text-default-400 text-small">$</span>
+                                                                    </div>
+                                                                }
+                                                            />
+                                                            <div>
+                                                                <p className="font-semibold text-sm">Actualizar imagen del producto</p>
+                                                                <input type="file" name="img" className="mt-2 block w-full text-sm text-slate-500
+                                                                    file:mr-4 file:py-2 file:px-4
+                                                                    file:rounded-full file:border-0
+                                                                    file:text-sm file:font-semibold
+                                                                    file:bg-amber-500 file:text-white
+                                                                    hover:file:bg-amber-600"
+                                                                    onChange={e => {
+                                                                        const file = e.target.files[0];
+                                                                        setImgActualizado(file);
+                                                                    }}
+                                                                />
+                                                            </div>
+                                                            <div>
+                                                            <Select 
+                                                                label="Actualizar categoria" 
+                                                                className="max-w-xs" 
+                                                                value={catActualizado}
+                                                                onChange={e => setCatActualizado(parseInt(e.target.value) + 1)}
+                                                            >
+                                                                {
+                                                                    itemCat.map((data, i)=> (
+                                                                        <SelectItem key={i} value={data.idcategoria}>
+                                                                            {data.nombre_categoria}
+                                                                        </SelectItem>
+                                                                    ))
+                                                                }
+                                                            </Select>
+                                                            </div>
+                                                        </div>
+                                                    </ModalBody>
+
+                                                    <ModalFooter className="mb-5 mx-10">
+                                                        <Button color="danger" variant="light" onPress={onClose}>
+                                                        Cancelar
+                                                        </Button>
+                                                        <Button color="primary" onClick={handleUpdate} onPress={onClose}>
+                                                        Actualizar
+                                                        </Button>
+                                                    </ModalFooter>
+                                                    </>
+                                                )}
+                                                </ModalContent>
+                                            </Modal>
+                                        </>
                                         <Button size="sm" color="danger">
                                             Eliminar
                                         </Button>
