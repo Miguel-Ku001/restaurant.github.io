77e0cead2000f1c1f09daba04ea5a3ca0dd38e9a
Nicolas <chaveznicolas3701@gmail.com>
2023-10-24 05:21:01 -0600

    Menu vista usuario y admin finalizado

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
 import {Link, Button} from "@nextui-org/react";
 import MenuItem from '/src/components/menu-item/MenuItem'
-import axios from "axios";
+import axios, { all } from "axios";
 
 export const Menu = () => {
 
   const [item, setItem] = useState([])
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [filterActive, setFilterActive] = useState(false);
   useEffect(()=> {
       axios.get('/api/items')
-      .then(res => setItem(res.data))
+      .then(res => {
+        setItem(res.data);
+        setFilteredItems(res.data);
+      })
       .catch(err => console.log(err));
   }, [])
+
+  const filterCategory = (category) => {
+    if (category === 0) {
+      setFilterActive(false);
+      setFilteredItems(item);
+    } else {
+      const filteredData = item.filter(data => data.idcategoria === category);
+      setFilterActive(true);
+      setFilteredItems(filteredData)
+    }
+    
+  }
 
   return (
     <div className="py-16 mr-36 font-marcellus">
@@ -18,11 +35,18 @@ export const Menu = () => {
           <h2 className="text-5xl text-center text-gray-800 font-medium">MENÚ</h2>
       </div>
 
-      
+      <div className="flex justify-center mb-10">
+        
+        <button className="mx-4 bg-[#cd9b4a] text-white px-4 py-2 rounded-lg active:scale-95 hover:scale-105 transition duration-500 shadow-lg" onClick={() => filterCategory(0)}> Todos </button>
+        <button className="mx-4 bg-[#cd9b4a] text-white px-4 py-2 rounded-lg active:scale-95 hover:scale-105 transition duration-500 shadow-lg" onClick={() => filterCategory(1)}> Comidas </button>
+        <button className="mx-4 bg-[#cd9b4a] text-white px-4 py-2 rounded-lg active:scale-95 hover:scale-105 transition duration-500 shadow-lg" onClick={() => filterCategory(2)}> Bebidas </button>
+        <button className="mx-4 bg-[#cd9b4a] text-white px-4 py-2 rounded-lg active:scale-95 hover:scale-105 transition duration-500 shadow-lg" onClick={() => filterCategory(3)}> Postres </button>
+        <button className="mx-4 bg-[#cd9b4a] text-white px-4 py-2 rounded-lg active:scale-95 hover:scale-105 transition duration-500 shadow-lg" onClick={() => filterCategory(4)}> Paquetes </button>
+      </div>
 
       <div className="grid gap-2 sm:grid-cols-1 lg:grid-cols-2 mb-12">
-        {item.map((data, i) => (
-          <MenuItem key={i} {...data}/>
+        {filteredItems.map((data, i) => (
+          <MenuItem key={data.id_items} {...data}/>
 
         ))}
       </div>
