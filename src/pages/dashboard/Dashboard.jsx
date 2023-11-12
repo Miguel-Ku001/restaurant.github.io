b30cc750210887082414a3da617ed4c5be62bec0
Miguel <miguelmaas91@gmail.com>
2023-11-11 19:20:18 -0600

    Graficas estaticas

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -6,53 +6,6 @@ import {
 } from 'recharts';
 import axios from "axios";
 
-const renderActiveShape = (props) => {
-    const RADIAN = Math.PI / 180;
-    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-    const sin = Math.sin(-RADIAN * midAngle);
-    const cos = Math.cos(-RADIAN * midAngle);
-    const sx = cx + (outerRadius + 10) * cos;
-    const sy = cy + (outerRadius + 10) * sin;
-    const mx = cx + (outerRadius + 30) * cos;
-    const my = cy + (outerRadius + 30) * sin;
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-    const ey = my;
-    const textAnchor = cos >= 0 ? 'start' : 'end';
-  
-    return (
-      <g>
-        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-          {payload.name}
-        </text>
-        <Sector
-          cx={cx}
-          cy={cy}
-          innerRadius={innerRadius}
-          outerRadius={outerRadius}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          fill={fill}
-        />
-        <Sector
-          cx={cx}
-          cy={cy}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          innerRadius={outerRadius + 6}
-          outerRadius={outerRadius + 10}
-          fill={fill}
-        />
-        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`PV ${value}`}</text>
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-          {`(Rate ${(percent * 100).toFixed(2)}%)`}
-        </text>
-      </g>
-    );
-  };
-
-
 export const Dashboard = () => {
 
     const data = [
@@ -107,6 +60,33 @@ export const Dashboard = () => {
         { name: 'Group E', value: 278 },
         { name: 'Group F', value: 189 },
     ];
+
+    // const [venta, setVenta] = useState([])
+    // useEffect(() => {
+    //     axios.get('/api/dashboard/venta/items')
+    //         .then(res => {
+    //             setVenta(res.data);
+    //         })
+    //         .catch(err => console.log(err));
+    // }, [])
+
+    const [items, setItems] = useState([])
+    useEffect(() => {
+        axios.get('/api/items')
+            .then(res => {
+                setItems(res.data);
+            })
+            .catch(err => console.log(err));
+    }, [])
+
+    const [cat, setCat] = useState([])
+    useEffect(() => {
+        axios.get('/api/items-categoria')
+            .then(res => {
+                setCat(res.data);
+            })
+            .catch(err => console.log(err));
+    }, [])
 
 
     return (
@@ -182,7 +162,7 @@ export const Dashboard = () => {
                     shadow="sm"
                 >
                     <CardBody>
-                        
+
                     </CardBody>
                 </Card>
             </div>
