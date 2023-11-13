598645e26e580bccc14d5311b70b6795ba1d7e9d
Miguel <miguelmaas91@gmail.com>
2023-11-12 20:49:27 -0600

    2 graficas finalizadas

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,57 +1,11 @@
 import React, { PureComponent, useEffect, useState } from "react";
 import { Card, CardBody } from "@nextui-org/react";
-import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
-    PieChart, Pie
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
+    PieChart, Pie, Sector
 } from 'recharts';
 import axios from "axios";
 
 export const Dashboard = () => {
-
-    const data = [
-        {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
     const data01 = [
         { name: 'Group A', value: 400 },
         { name: 'Group B', value: 300 },
@@ -61,14 +15,63 @@ export const Dashboard = () => {
         { name: 'Group F', value: 189 },
     ];
 
-    // const [venta, setVenta] = useState([])
-    // useEffect(() => {
-    //     axios.get('/api/dashboard/venta/items')
-    //         .then(res => {
-    //             setVenta(res.data);
-    //         })
-    //         .catch(err => console.log(err));
-    // }, [])
+    const [venta, setVenta] = useState([])
+    useEffect(() => {
+        axios.get('/api/dashboard/venta/items')
+            .then(res => {
+                setVenta(res.data);
+            })
+            .catch(err => console.log(err));
+    }, [])
+
+// Graficas de productos más y menos vendidos
+    const agregadosData = venta.reduce((result, item) => {
+        const existenteItem = result.find(entry => entry.id_items === item.id_items);
+
+        if (existenteItem) {
+            existenteItem.cantidad += item.cantidad;
+        } else {
+            result.push({ id_items: item.id_items, cantidad: item.cantidad, nombre: item.nombre });
+        }
+        return result;
+    }, []);
+
+    //Ordenar por cantidad de manera descendente
+    const ordenVentaDesc = [...agregadosData].sort((a, b) => b.cantidad - a.cantidad);
+
+    // tomar los 5 primeros datos al ordenar
+    const top5Items = ordenVentaDesc.slice(0, 5);
+
+    const data = top5Items.map(item => ({
+        nombre: item.nombre,
+        cantidad: item.cantidad,
+    }));
+
+    //Ordenar por cantidad de manera ascendete
+    const ordenVentaAsc = [...agregadosData].sort((a, b) => a.cantidad - b.cantidad);
+    const top5Items2 = ordenVentaAsc.slice(0, 5);
+
+    const data2 = top5Items2.map(item => ({
+        nombre: item.nombre,
+        cantidad: item.cantidad,
+    }));
+
+// Fin de graficas de productos más y menos vendidos
+
+
+    const [categorias, setCategorias] = useState([]);
+    useEffect(() => {
+        axios.get('/api/dashboard/categorias')
+            .then(res => {
+                setCategorias(res.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+
+// Categorías más vendidas
+
+// Fin de categorías más vendidas
 
     const [items, setItems] = useState([])
     useEffect(() => {
@@ -95,37 +98,62 @@ export const Dashboard = () => {
             <div className="mb-10">
                 <h2 className="text-5xl text-center text-gray-800">DASHBOARD</h2>
             </div>
-            <div className="flex gap-8">
+            <div className="flex gap-8 h-[25rem] w-full">
                 <Card
                     isBlurred
-                    className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
-                    shadow="sm"
+                    className="border-none bg-background/60 dark:bg-default-100/50 w-1/2"
+                    // shadow="sm"
                 >
                     <CardBody>
-                        <ResponsiveContainer width="100%" height="100%">
-                            <BarChart width={150} height={40} data={data}>
-                                <XAxis dataKey="name" />
+                        <div className="text-center text-xl">Productos más vendidos</div>
+                        <ResponsiveContainer width="90%" height="90%">
+                            <BarChart 
+                                width={500} 
+                                height={300} 
+                                data={data}
+                                margin={{
+                                    top: 10,
+                                    right: 10,
+                                    left: 10,
+                                    bottom: 5,
+                                  }}
+                                // margin={{ top: 5, bottom: 5 }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="nombre" className="text-xs"/>
                                 <YAxis />
                                 <Tooltip />
-                                <Legend />
-                                <Bar dataKey="uv" fill="#8884d8" />
+                                {/* <Legend /> */}
+                                <Bar dataKey="cantidad" fill="#cd9b4a" />
                             </BarChart>
                         </ResponsiveContainer>
                     </CardBody>
                 </Card>
                 <Card
                     isBlurred
-                    className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
-                    shadow="sm"
+                    className="border-none bg-background/60 dark:bg-default-100/50 w-1/2"
+                    // shadow="sm"
                 >
                     <CardBody>
-                        <ResponsiveContainer width="100%" height="100%">
-                            <BarChart width={150} height={40} data={data}>
-                                <XAxis dataKey="name" />
+                        <div className="text-center text-xl">Productos menos vendidos</div>
+                        <ResponsiveContainer width="90%" height="90%">
+                            <BarChart 
+                                width={500} 
+                                height={300} 
+                                data={data2}
+                                margin={{
+                                    top: 10,
+                                    right: 10,
+                                    left: 10,
+                                    bottom: 5,
+                                  }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="nombre" className="text-xs" />
                                 <YAxis />
                                 <Tooltip />
-                                <Legend />
-                                <Bar dataKey="uv" fill="#8884d8" />
+                                {/* <Legend /> */}
+                                <Bar dataKey="cantidad" fill="#cd9b4a" />
                             </BarChart>
                         </ResponsiveContainer>
                     </CardBody>
@@ -135,15 +163,15 @@ export const Dashboard = () => {
                 <Card
                     isBlurred
                     className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
-                    shadow="sm"
+                    // shadow="sm"
                 >
                     <CardBody>
                         <ResponsiveContainer width="100%" height="100%">
                             <PieChart width={400} height={400}>
                                 <Pie
-                                    dataKey="value"
+                                    dataKey="nombre_categoria"
                                     isAnimationActive={false}
-                                    data={data01}
+                                    data={categorias}
                                     cx="50%"
                                     cy="50%"
                                     outerRadius={120}
@@ -159,10 +187,10 @@ export const Dashboard = () => {
                 <Card
                     isBlurred
                     className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
-                    shadow="sm"
+                    // shadow="sm"
                 >
                     <CardBody>
-
+                        
                     </CardBody>
                 </Card>
             </div>
