6c17ac516afcf7214ddad23e049238e9548ba021
Miguel <miguelmaas91@gmail.com>
2023-11-19 15:09:25 -0600

    Estilos de graficas v2

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React, { PureComponent, useEffect, useState } from "react";
-import { Card, CardBody } from "@nextui-org/react";
-import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
-    PieChart, Pie, Sector
+import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import { MdFoodBank, MdViewCarousel, MdSupervisorAccount } from "react-icons/md";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
+        PieChart, Pie, Sector
 } from 'recharts';
 import axios from "axios";
 
@@ -20,41 +20,41 @@ const renderActiveShape = (props) => {
     const ex = mx + (cos >= 0 ? 1 : -1) * 22;
     const ey = my;
     const textAnchor = cos >= 0 ? 'start' : 'end';
-  
-    return (
-      <g>
-        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-          {payload.nombre_categoria}
-        </text>
-        <Sector
-          cx={cx}
-          cy={cy}
-          innerRadius={innerRadius}
-          outerRadius={outerRadius}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          fill={fill}
-        />
-        <Sector
-          cx={cx}
-          cy={cy}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          innerRadius={outerRadius + 6}
-          outerRadius={outerRadius + 10}
-          fill={fill}
-        />
-        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Cantidad ${value}`}</text>
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-          {`(${(percent * 100).toFixed(2)}%)`}
-        </text>
-      </g>
-    );
-  };
 
-  
+    return (
+        <g>
+            <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+                {payload.nombre_categoria}
+            </text>
+            <Sector
+                cx={cx}
+                cy={cy}
+                innerRadius={innerRadius}
+                outerRadius={outerRadius}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                fill={fill}
+            />
+            <Sector
+                cx={cx}
+                cy={cy}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                innerRadius={outerRadius + 6}
+                outerRadius={outerRadius + 10}
+                fill={fill}
+            />
+            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Cantidad ${value}`}</text>
+            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+                {`(${(percent * 100).toFixed(2)}%)`}
+            </text>
+        </g>
+    );
+};
+
+
 export const Dashboard = () => {
     const data01 = [
         { name: 'Group A', value: 400 },
@@ -119,7 +119,10 @@ export const Dashboard = () => {
             .catch(err => console.log(err));
     }, []);
 
-
+    const [activeIndex, setActiveIndex] = useState(0);
+    const onPieEnter = (_, index) => {
+        setActiveIndex(index);
+    };
     // Categorías más vendidas
 
     // Fin de categorías más vendidas
@@ -154,27 +157,18 @@ export const Dashboard = () => {
             .catch(err => console.log(err));
     }, [])
 
-
-    const [activeIndex, setActiveIndex] = useState(0);
-    const onPieEnter = (_, index) => {
-        setActiveIndex(index);
-    };
+    const [reservation, setReservation] = useState([])
+    useEffect(() => {
+        axios.get('/api/reservacion')
+            .then(res => {
+                const reser = res.data.filter(reservacion_servicio => reservacion_servicio.observacion === 1);
+                const observationreser = reser.length;
+                setReservation(observationreser);
+            })
+            .catch(err => console.log(err));
+    }, [])
     
-    
-    const [visitantes, setVisitantes] = useState(0);
 
-  useEffect(() => {
-    const incrementarContador = () => {
-      axios.post('/api/track-visitor')
-        .then(() => {
-          setVisitantes((prev) => prev + 1);
-        })
-        .catch((error) => {
-          console.error('Error al incrementar el contador de visitantes', error);
-        });
-    };
-    incrementarContador();
-  }, []);
 
     return (
 
@@ -183,41 +177,92 @@ export const Dashboard = () => {
                 <h2 className="text-5xl text-center text-gray-800">DASHBOARD</h2>
             </div>
             <div>
+
                 <div className="flex gap-8 h-26 w-full">
                     <Card
                         isBlurred
-                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/4"
+                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/3"
                     >
                         <CardBody>
-                            <div className="text-center text-xl">Total de clientes</div>
-                            <div className="text-3xl font-bold text-center text-gray-800">{user}</div>                       
+                            <div className="flex px-4">
+                                <div className="w-3/4 pr-2">
+                                    <div className="text-xl">Ordenes en proceso</div>
+                                    <div className="text-3xl text-gray-800 ml-4">2</div>
+                                </div>
+                                <div className="w-1/4 flex items-center justify-center">
+                                    <MdFoodBank color="#fb923c" size="4em" className="text-center items-center"/>
+                                </div>
+                            </div>                          
                         </CardBody>
                     </Card>
 
                     <Card
                         isBlurred
-                        className="border-none bg-background/60 dark:bg-default-100/50 w-2/4"
+                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/3"
                     >
                         <CardBody>
-                            <div className="text-center text-2xl">GUSSAB</div>
-                            <div className="text-center text-xl">¡Donde el gusto y el sabor se unen!</div>
+                            <div className="flex px-4">
+                                <div className="w-3/4 pr-2">
+                                    <div className="text-xl">Reservaciones</div>
+                                    <div className="text-3xl text-gray-800 ml-4">{reservation}</div>
+                                </div>
+                                <div className="w-1/4 flex items-center justify-center">
+                                    <MdViewCarousel color="#fb923c" size="4em" className="text-center items-center"/>
+                                </div>
+                            </div>
                         </CardBody>
                     </Card>
 
-                    <Card
+                    {/* <Card
                         isBlurred
-                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/4"
+                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/3"
                     >
                         <CardBody>
-                            <div className="text-center text-xl">Numero de visitantes</div>
-                            <div className="text-3xl font-bold text-center text-gray-800">{visitantes}</div> 
+                            <div className="text-center text-xl">Ordenes en </div>
+                            <div className="text-3xl font-bold text-center text-gray-800">{user}</div>
+                        </CardBody>
+                    </Card> */}
+
+                    <Card
+                        isBlurred
+                        className="border-none bg-[background/60] dark:bg-default-100/50 w-1/3"
+                    >
+                        <CardBody>
+                            <div className="flex px-4">
+                                <div className="w-3/4 pr-2">
+                                    <div className="text-xl">Total de clientes</div>
+                                    <div className="text-3xl text-gray-800 ml-4">{user}</div>
+                                </div>
+                                <div className="w-1/4 flex items-center justify-center">
+                                    <MdSupervisorAccount color="#fb923c" size="4em" className="text-center items-center"/>
+                                </div>
+                            </div>
                         </CardBody>
                     </Card>
                 </div>
                 <div className="flex gap-8 h-[25rem] w-full pt-12">
+                    <Card 
+                        isBlurred
+                        className="border-none bg-[#2c3033] dark:bg-default-100/50 w-1/3 py-4"
+                    >       
+                        <CardHeader className="text-center pb-0 pt-2 px-4 flex-col">
+                            <h4 className="text-3xl text-white">GUSSAB</h4>
+                        </CardHeader>  
+                        <CardBody className="overflow-visible py-4 items-center">
+                            <Image
+                                alt="Card background"
+                                className="object-cover rounded-xl"
+                                src="./src/images/vista-inicio/loguito.png"
+                                width={140}
+                            />
+                        </CardBody>
+                        <CardHeader className="text-center pb-2 pt-0 px-4 flex-col">
+                            <p className="text-xl text-white">¡Donde el buen gusto se une al sabor!</p>
+                        </CardHeader>
+                    </Card>
                     <Card
                         isBlurred
-                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/2"
+                        className="border-none bg-background/60 dark:bg-default-100/50 w-2/3"
                     // shadow="sm"
                     >
                         <CardBody>
@@ -229,8 +274,8 @@ export const Dashboard = () => {
                                     data={data}
                                     margin={{
                                         top: 10,
-                                        right: 10,
-                                        left: 10,
+                                        right: 15,
+                                        left: 25,
                                         bottom: 5,
                                     }}
                                 // margin={{ top: 5, bottom: 5 }}
@@ -240,8 +285,45 @@ export const Dashboard = () => {
                                     <YAxis />
                                     <Tooltip />
                                     {/* <Legend /> */}
-                                    <Bar dataKey="cantidad" fill="#06b6d4" />
+                                    <Bar dataKey="cantidad" fill="#fb923c" />
                                 </BarChart>
+                            </ResponsiveContainer>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="flex pt-12 gap-8">
+                    <Card
+                        isBlurred
+                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
+                    // shadow="sm"
+                    >
+                        <CardBody>
+                            <div className="text-center text-xl">Categorías más vendidas</div>
+                            <ResponsiveContainer width="100%" height="100%">
+                                <PieChart width={400} height={400}>
+                                    <Pie
+                                        activeIndex={activeIndex}
+                                        activeShape={renderActiveShape}
+                                        dataKey="total_cantidad"
+                                        // isAnimationActive={false}
+                                        data={categorias}
+                                        cx="50%"
+                                        cy="50%"
+                                        innerRadius={80}
+                                        outerRadius={105}
+                                        fill="#fb923c"
+                                        // label
+                                        onMouseEnter={onPieEnter}
+                                    // nameKey="nombre_categoria"
+                                    >
+                                        {/* {
+                                            categorias.map((categoria, index) => (
+                                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                            ))
+                                        } */}
+                                    </Pie>
+                                    {/* <Tooltip /> */}
+                                </PieChart>
                             </ResponsiveContainer>
                         </CardBody>
                     </Card>
@@ -269,56 +351,9 @@ export const Dashboard = () => {
                                     <YAxis />
                                     <Tooltip />
                                     {/* <Legend /> */}
-                                    <Bar dataKey="cantidad" fill="#06b6d4" />
+                                    <Bar dataKey="cantidad" fill="#fb923c" />
                                 </BarChart>
                             </ResponsiveContainer>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="flex pt-12 gap-8">
-                    <Card
-                        isBlurred
-                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
-                    // shadow="sm"
-                    >
-                        <CardBody>
-                        <div className="text-center text-xl">Categorías más vendidas</div>
-                            <ResponsiveContainer width="100%" height="100%">
-                                <PieChart width={400} height={400}>
-                                    <Pie
-                                        activeIndex={activeIndex}
-                                        activeShape={renderActiveShape}
-                                        dataKey="total_cantidad"
-                                        // isAnimationActive={false}
-                                        data={categorias}
-                                        cx="50%"
-                                        cy="50%"
-                                        innerRadius={80}
-                                        outerRadius={105}
-                                        fill="#06b6d4"
-                                        // label
-                                        onMouseEnter={onPieEnter}
-                                        // nameKey="nombre_categoria"
-                                    >
-                                        {/* {
-                                            categorias.map((categoria, index) => (
-                                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                                            ))
-                                        } */}
-                                    </Pie>
-                                    {/* <Tooltip /> */}
-                                </PieChart>
-                            </ResponsiveContainer>
-                        </CardBody>
-                    </Card>
-                    <Card
-                        isBlurred
-                        className="border-none bg-background/60 dark:bg-default-100/50 w-1/2 h-96"
-                    // shadow="sm"
-                    >
-                        <CardBody>
-                            <div className="text-center text-xl">Clientes Registrados</div>
-                            <div className="text-4xl font-bold text-center text-gray-800">{user}</div>                                        
                         </CardBody>
                     </Card>
                 </div>
